e1b1a32042cc5ed4850af37a2790faafb3b24cbf
Junzhe Zhang <junzhez@junzhez.com>
2014-07-15 08:27:44 -0700

    first commit

---
+++ DeepLearningLibrary/DeepLearningLibrary/NeuralNetwork.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace DeepLearningLibrary
+{
+    public delegate double ActivationFunc(double input);
+
+    public class NeuralNetwork
+    {
+        private List<NeuralLayer> layers;
+        internal List<NeuralLayer> Layers
+        {
+            get { return layers; }
+            set { layers = value; }
+        }
+
+        private ActivationFunc func;
+        public ActivationFunc Func
+        {
+            get { return func; }
+            set { func = value; }
+        }
+
+        public NeuralNetwork(int[] num_layers, ActivationFunc _func)
+        {
+            func = _func;
+
+            initLayers(num_layers);
+        }
+
+        private void initLayers(int[] num_layers)
+        {
+            layers = new List<NeuralLayer>();
+
+            for (int i = 0; i < num_layers.Length - 1; i++)
+            {
+                Matrix<double> weight = new SparseMatrix(num_layers[i], num_layers[i + 1]);
+
+                layers.Add(new NeuralLayer(weight, func));
+            }
+        }
+
+        public Matrix<double> forwardFeed(Matrix<double> input)
+        {
+            for (int i = 0; i < layers.Count; i++)
+            {
+                input = layers[i].forwardFeed(input);
+            }
+
+            return layers[layers.Count - 1].Output;
+        }
+
+        public void backPropagate(Matrix<double> output)
+        {
+            Matrix<double> sig = output.Add(layers[layers.Count - 1].Output.Multiply(-1));
+
+            for (int i = layers.Count - 1; i >= 0; i--)
+            {
+                sig = layers[i].backPropagate(sig);
+            }
+        }
+
+        public void updateDerivative()
+        {
+            foreach (var layer in layers)
+                layer.updateDerivative();
+        }
+
+        public void train(Matrix<double> input, Matrix<double> output)
+        {
+            forwardFeed(input);
+            backPropagate(output);
+            updateDerivative();
+        }
+
+        public void applyDerivative(int m, double miu, double alpha)
+        {
+            foreach (var layer in layers)
+                layer.applyDerivative(m, miu, alpha);
+        }
+
+
+    }
+}
---
+++ DeepLearningLibrary/Mnist/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using DeepLearningLibrary;
+
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Mnist
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start");
+            FileStream ifsLabels =
+              new FileStream(@"F:\Data\train-labels.idx1-ubyte",
+                 FileMode.Open); // test labels
+                FileStream ifsImages =
+                 new FileStream(@"F:\Data\train-images.idx3-ubyte",
+                 FileMode.Open); // test images
+
+                BinaryReader brLabels =
+                 new BinaryReader(ifsLabels);
+                BinaryReader brImages =
+                 new BinaryReader(ifsImages);
+
+                int magic1 = brImages.ReadInt32(); // discard
+                int numImages = brImages.ReadInt32();
+                int numRows = brImages.ReadInt32();
+                int numCols = brImages.ReadInt32();
+
+                int magic2 = brLabels.ReadInt32();
+                int numLabels = brLabels.ReadInt32();
+
+                Matrix<double> input = new SparseMatrix(60000, 28*28);
+
+                Matrix<double> output = new SparseMatrix(60000, 10);
+
+                int[] numlayers = { 28 * 28, 200, 10 };
+
+                NeuralNetwork nn = new NeuralNetwork(numlayers, Math.Tanh);
+
+                for(int di = 0; di < 60000; di++)
+                {
+                    double[] image = new double[28 * 28];
+                    double[] label = new double[10];
+
+                    for(int i = 0; i < 28; i++)
+                    {
+                        for(int j = 0; j < 28; j++)
+                        {
+                            image[i * 28 + j] = brImages.ReadByte();
+                        }
+                    }
+
+                    int ind = brLabels.ReadByte();
+
+                    label[ind] = 1;
+
+                    input.SetRow(di, image);
+                    output.SetRow(di, label);
+
+                    Console.WriteLine(di);
+                }
+
+
+                for (int dj = 0; dj < 1000; dj++)
+                {
+                    for (int di = 0; di < 60000; di++)
+                    {
+                        nn.train(input.SubMatrix(di, 1, 0, 28 * 28), output.SubMatrix(di, 1, 0, 10));
+                    }
+                    nn.applyDerivative(numImages, 1, 0.01);
+                }
+               
+                brImages.Close();
+                brLabels.Close();
+
+                ifsLabels =
+              new FileStream(@"F:\Data\t10k-labels.idx1-ubyte",
+                 FileMode.Open); // test labels
+                ifsImages =
+                 new FileStream(@"F:\Data\t10k-images.idx3-ubyte",
+                 FileMode.Open);
+
+                brLabels =
+                     new BinaryReader(ifsLabels);
+                brImages =
+                 new BinaryReader(ifsImages);
+
+                magic1 = brImages.ReadInt32(); // discard
+                numImages = brImages.ReadInt32();
+                numRows = brImages.ReadInt32();
+                numCols = brImages.ReadInt32();
+
+                magic2 = brLabels.ReadInt32();
+                numLabels = brLabels.ReadInt32();
+
+                input = new SparseMatrix(1, 28 * 28);
+                output = new SparseMatrix(1, 10);
+
+                int count = 0;
+
+                for (int di = 0; di < 10000; di++)
+                {
+                    double[] image = new double[28 * 28];
+                    double[] label = new double[10];
+
+                    for (int i = 0; i < 28; i++)
+                    {
+                        for (int j = 0; j < 28; j++)
+                        {
+                            image[i * 28 + j] = brImages.ReadByte();
+                        }
+                    }
+
+                    int ind = brLabels.ReadByte();
+
+                    label[ind] = 1;
+
+                    input.SetRow(0, image);
+                    output.SetRow(0, label);
+
+                    Matrix<double> tmp_out = nn.forwardFeed(input);
+
+                    double max = Double.MinValue;
+                    int dl = 0;
+                    for (int dj = 0; dj < tmp_out.ColumnCount; dj++)
+                    {
+                        if(max < tmp_out[0, dj])
+                        {
+                            max = tmp_out[0, dj];
+                            dl = dj;
+                        }
+                    }
+
+                    if (ind == dl)
+                        count++;
+
+                    Console.WriteLine(di);
+                }
+                
+                double rate =  count / 10000;
+
+                Console.WriteLine(rate);
+                
+                Console.WriteLine("End");
+                Console.ReadLine();
+        } 
+    } 
+}
